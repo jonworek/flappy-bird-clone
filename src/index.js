376ab1dc3810fe25ac4f46135e29d724d57f734f
Jon Worek <jworek@gmail.com>
2024-12-11 18:36:10 -0500

    Plays around with the code a little to understand init better

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 
 import Phaser from "phaser";
 
+
+
+/* 
+  The preload function is called by Phaser at the start of the game. 
+  It is used to load assets like images, audio files, and other resources. 
+  In this case, we are loading an image called sky.png.
+*/
+const preload = function() {
+  this.load.image('sky', 'assets/sky.png');
+}
+
+/*
+  The create function is called by Phaser after the preload function. 
+  It is used to create game objects like sprites, text, and shapes. 
+  In this case, we are creating a sprite with the sky image.
+*/
+const create = function() {
+  this.add.image(400, 300, 'sky');
+}
+
+const update = function() {
+  // TODO
+}
+
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.AUTO,  // auto is probably going to be WebGL
   width: 800,
   height: 600,
   physics: {
@@ -12,17 +36,10 @@ const config = {
     }
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
+    //update,
   }
 };
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
